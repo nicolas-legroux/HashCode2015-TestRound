6bda239940928940841914dfb7b13729fd8e1629
Nicolas Legroux <nicolas.legroux@polytechnique.edu>
2015-03-27 21:00:55 +0100

    Writing solution to file

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.HashSet;
 
 
@@ -6,7 +9,25 @@ public class Solution {
 	HashSet<Part> parts;
 
 	void save(String filename) {
-		;
+		File file = new File(filename);
+		
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(String.valueOf(parts.size() + "\n"));
+		
+		for(Part part : parts){
+			bw.write(String.valueOf(part.top) + " " + 
+					String.valueOf(part.left) + " " +
+					String.valueOf(part.bottom) + " " +
+					String.valueOf(part.right));
+		}	
+		
+		bw.close();
+		
+		System.out.println("Done writing");
 	}
-
 }
