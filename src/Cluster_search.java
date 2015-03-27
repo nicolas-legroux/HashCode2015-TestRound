ed50d64680841f6e941f1582c63694605e627b21
Claire Donnat <clairedonnat@Claires-MacBook-Air.local>
2015-03-27 22:12:54 +0100

    test

--- src/Cluster_search.java
+++ src/Cluster_search.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Set;
 
 public class Cluster_search {
@@ -6,7 +7,7 @@ public class Cluster_search {
 	Set<Cluster> cluster_set;
 	
 	
-	public Cluster_search(){
+	public Cluster_search() throws IOException{
 		
 		pb.load("data/test_round.in");
 		clusters=new int[pb.height][pb.width];
@@ -16,14 +17,43 @@ public class Cluster_search {
 		for (int i=0;i<pb.height;i++){
 			for (int j=0;j<pb.width;j++){
 				if(clusters[i][j]==0 && pb.map[i][j].equals(Problem.Type.Jambon)){
-					trouverCluster(i,j);
+					trouverCluster(i,j, cluster_compt);
 				}
 					
 				}
 			}
 		}
 		
-	public void trouverCluster(int i, int j){
+	public void trouverCluster(int a, int b, int compt){
+		int ham_compt=0;
+		Cluster c=new Cluster(a, b, a,b,a, b);
+		for (int i=0;i<6;i++){
+			for (int j=0;j<6;j++){
+				if (ham_compt<3){
+					if(pb.map[a+i][b+j].equals(Problem.Type.Jambon)){
+						ham_compt++;
+						clusters[i][j]=compt;
+						if (i<c.r1 && ham_compt==2){
+							c.r2=c.r1;
+							c.c2=c.c1;
+							
+						}
+						
+						if (i<c.r1 && ham_compt==3){
+							c.r3=c.r1;
+							c.c3=c.c1;
+						
+						}
+				
+						
+						c.r1=i;
+						c.r2=j;
+						
+					}
+				}
+			}
+			 
+		}
 		
 	}
 		
