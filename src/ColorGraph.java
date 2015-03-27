2a354ce7c053a5acb23cb805e656d9ed514cc4d0
G. Endignoux <ggendx@gmail.com>
2015-03-27 21:20:01 +0100

    Color graph

--- src/ColorGraph.java
+++ src/ColorGraph.java
@@ -2,6 +2,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map.Entry;
+import java.util.PriorityQueue;
 
 
 public class ColorGraph {
@@ -23,12 +24,28 @@ public class ColorGraph {
 	
 	Solution color(Graph g) {
 		Solution solution = new Solution();
+
+		PriorityQueue<Part> queue = new PriorityQueue<Part>(g.graph.size(), new CompareParts());
 		
 		for (Entry<Part, HashSet<Part>> e : g.graph.entrySet()) {
 			neighbors.put(e.getKey(), e.getValue().size());
+			queue.add(e.getKey());
 		}
 		
-		;
+		while (!queue.isEmpty()) {
+			Part p = queue.poll();
+			HashSet<Part> n = g.graph.remove(p);
+			
+			for (Part part : n) {
+				queue.remove(part);
+				if (!g.graph.get(part).remove(p))
+					System.err.println("Missing edge !");
+				neighbors.put(part, neighbors.get(part) - 1);
+				queue.add(part);
+			}
+			
+			solution.parts.add(p);
+		}
 		
 		return solution;
 	}
