be1d611bb77307dc7abfda929ed1386ddfe05460
G. Endignoux <ggendx@gmail.com>
2015-03-27 20:50:24 +0100

    Skeleton graph

---
+++ src/Graph.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+public class Graph {
+	
+	HashMap<Part, HashSet<Part> > graph;
+	
+	Graph() {
+		graph = new HashMap<Part, HashSet<Part> >();
+	}
+	
+	void addPair(Part p1, Part p2) {
+		// TODO
+	}
+
+}
