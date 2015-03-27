8d5eafac6505dc50af51ecd02e89b72faaf4ea40
Nicolas Legroux <nicolas.legroux@polytechnique.edu>
2015-03-27 21:46:56 +0100

    Cluster class

---
+++ src/Cluster.java
@@ -0,0 +1,19 @@
+
+public class Cluster {
+	
+	int r1;
+	int r2;
+	int r3;
+	int c1;
+	int c2;
+	int c3;
+	
+	Cluster(int _r1, int _r2, int _r3, int _c1, int _c2, int _c3){
+		r1=_r1;
+		r2=_r2;
+		r3=_r3;
+		c1=_c1;
+		c2=_c2;
+		c3=_c3;
+	}
+}
