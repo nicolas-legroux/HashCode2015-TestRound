95615ec61d49c5d3d764c1d86a2fa49856976fb2
G. Endignoux <ggendx@gmail.com>
2015-03-27 20:53:58 +0100

    Graph

--- src/Graph.java
+++ src/Graph.java
@@ -10,8 +10,18 @@ public class Graph {
 		graph = new HashMap<Part, HashSet<Part> >();
 	}
 	
+	private void addLink(Part p1, Part p2) {
+		HashSet<Part> s1 = graph.get(p1);
+		if (s1 == null) {
+			s1 = new HashSet<Part>();
+			graph.put(p1, s1);
+		}
+		s1.add(p2);
+	}
+	
 	void addPair(Part p1, Part p2) {
-		// TODO
+		addLink(p1, p2);
+		addLink(p2, p1);
 	}
 
 }
